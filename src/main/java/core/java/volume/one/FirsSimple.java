9909c8fa7ad4a7506c3e5cea06c82897b115b366
test_1234 <490055850@qq.com>
2021-07-10 22:21:13 +0800

    在线编辑

--- src/main/java/core/java/volume/one/FirsSimple.java
+++ src/main/java/core/java/volume/one/FirsSimple.java
@@ -4,6 +4,7 @@ public class FirsSimple {
     public static void main(String[] args) {
         System.out.println("版本1");
         System.out.println("创建分支");
-        System.out.println("创建dev-02分支");
+        System.out.println("创建dev-02分支"); 
+        System.out.println("在线编辑");
     }
 }
