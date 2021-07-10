44830c49524117b5beddc786c2733951ccd63808
yangshuaifeng <QWEasd123$%^>
2021-07-10 18:45:17 +0800

    创建分支

--- src/main/java/core/java/volume/one/FirsSimple.java
+++ src/main/java/core/java/volume/one/FirsSimple.java
@@ -3,5 +3,6 @@ package core.java.volume.one;
 public class FirsSimple {
     public static void main(String[] args) {
         System.out.println("版本1");
+        System.out.println("创建分支");
     }
 }
